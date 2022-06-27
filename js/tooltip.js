09ca1bd1db46015ebae4257c9883317895b514f2
SigreRabbit <105998848+SigreRabbit@users.noreply.github.com>
2022-06-27 20:13:08 +0900

    Update tooltip.js

--- js/tooltip.js
+++ js/tooltip.js
@@ -15,20 +15,23 @@ $(function () {
             function (e) {
                 var tip = $(this).children(".tip")
 
-                var mousex = e.pageX + 20; //マウスの位置（X座標）を取得
-                var mousey = e.pageY + 20; //マウスの位置（Y座標）を取得
-                var tipWidth = tip.width(); //ツールチップの幅を取得
-                var tipHeight = tip.height(); //ツールチップの高さを取得
+                var tipLeft = e.pageX;
+                var tipTop = e.pageY;
 
-                var tipVisX = $(window).width() - (mousex + tipWidth);
-                var tipVisY = $(window).height() - (mousey + tipHeight);
-
-                if (tipVisX < 20) { //画面幅を超えた場合はX座標を調節
-                    mousex = e.pageX - tipWidth - 20;
-                } if (tipVisY < 20) { //画面高さを超えた場合はY座標を調節
-                    mousey = e.pageY - tipHeight - 20;
+                var mouseX = e.clientX;
+                var mouseY = e.clientY;
+                var tipWidth = tip.width();
+                var tipHeight = tip.height();
+                var tipVisX = $(window).width() - (mouseX + tipWidth);
+                var tipVisY = $(window).height() - (mouseY + tipHeight);
+                if (tipVisX < 0) { //画面幅を超えた場合はX座標を調節
+                    tipLeft = tipLeft - tipWidth;
                 }
-                tip.css({ top: mousey, left: mousex });
+                if (tipVisY < 0) { //画面高さを超えた場合はY座標を調節
+                    tipTop = tipTop - tipHeight;
+                }
+
+                tip.css({ left: tipLeft, top: tipTop });
             }
         );
 });
